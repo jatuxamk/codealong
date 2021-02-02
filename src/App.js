fa4f5e769c11bbbfb059461fde4e85fa0567d2b9
Janne Turunen <janne.turunen@xamk.fi>
2021-02-02 12:18:44 +0200

    neljas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import { useState, useEffect } from 'react';
+import { Modal } from 'react-bootstrap'; 
 
 function App() {
 
+  const [naytaModal, setNaytaModal] = useState(false);
   const [data, setData] = useState({
                                       kayttajat : [],
                                       tiedotHaettu : false,
                                       virhe : null
                                   });
+
+  const lisaysDialogi = () => {
+
+    setNaytaModal(true);
+
+  }
+
+  const muokkausDialogi = () => {
+
+    setNaytaModal(true);
+
+  }
+
+  const poistoDialogi = () => {
+
+    setNaytaModal(true);
+
+  }  
 
   const haeTiedot = async () => {
 
@@ -41,18 +61,20 @@ function App() {
 
   return (
     <div>
+
       <nav className="navbar navbar-light bg-light mb-3">
         <div className="container">
           <span className="navbar-brand mb-0 h1">
             Käyttäjärekisteri
           </span>
-          <button className="btn btn-light">Lisää uusi käyttäjä</button>
+          <button 
+            className="btn btn-light"
+            onClick={lisaysDialogi}
+          >Lisää uusi käyttäjä</button>
         </div>
       </nav>
 
       <div className="container">
-
-      <i className="bi bi-alarm"></i>
 
         {(data.virhe)
           ? <div className="alert alert-danger">
@@ -65,6 +87,8 @@ function App() {
                     <th>Nimi</th>
                     <th>Käyttäjätunnus</th>
                     <th>Sähköposti</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -73,6 +97,22 @@ function App() {
                               <td>{kayttaja.sukunimi}, {kayttaja.etunimi}</td>
                               <td>{kayttaja.sahkoposti}</td>
                               <td>{kayttaja.kayttajatunnus}</td>
+                              <td>
+                                  <button 
+                                    className="btn btn-link"
+                                    onClick={muokkausDialogi}
+                                  >
+                                    <i class="bi bi-pencil-square"></i>
+                                  </button>
+                              </td>
+                              <td>
+                                  <button 
+                                    className="btn btn-link"
+                                    onClick={poistoDialogi}
+                                  >
+                                    <i class="bi bi-trash"></i>
+                                  </button>
+                              </td>
                            </tr>
                   })}                  
                 </tbody>
@@ -84,6 +124,13 @@ function App() {
         
 
       </div>
+
+      <Modal show={naytaModal}>
+        <Modal.Body>
+          <h1>JEE!</h1>
+        </Modal.Body>
+      </Modal>
+
     </div>
   );
 }
